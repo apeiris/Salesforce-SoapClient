3f74e9694bb79c4816c020d0125336e3c81dff75
apeiris <mapeiris@hotmail.com>
2025-01-28 12:25:15 -0500

    test

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,12 +32,17 @@
             statusStrip1 = new StatusStrip();
             toolStripStatusLabel1 = new ToolStripStatusLabel();
             btnCallSoap = new Button();
+            tabControl1 = new TabControl();
+            tbpScaffold = new TabPage();
+            tbpAccounts = new TabPage();
             statusStrip1.SuspendLayout();
+            tabControl1.SuspendLayout();
+            tbpScaffold.SuspendLayout();
             SuspendLayout();
             // 
             // btnGetAccessToken
             // 
-            btnGetAccessToken.Location = new Point(0, 437);
+            btnGetAccessToken.Location = new Point(275, 290);
             btnGetAccessToken.Name = "btnGetAccessToken";
             btnGetAccessToken.Size = new Size(134, 23);
             btnGetAccessToken.TabIndex = 0;
@@ -62,7 +67,7 @@
             // 
             // btnCallSoap
             // 
-            btnCallSoap.Location = new Point(39, 314);
+            btnCallSoap.Location = new Point(75, 290);
             btnCallSoap.Name = "btnCallSoap";
             btnCallSoap.Size = new Size(158, 23);
             btnCallSoap.TabIndex = 2;
@@ -70,19 +75,53 @@
             btnCallSoap.UseVisualStyleBackColor = true;
             btnCallSoap.Click += btnCallSoap_Click;
             // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tbpScaffold);
+            tabControl1.Controls.Add(tbpAccounts);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1379, 463);
+            tabControl1.TabIndex = 3;
+            // 
+            // tbpScaffold
+            // 
+            tbpScaffold.Controls.Add(btnCallSoap);
+            tbpScaffold.Controls.Add(btnGetAccessToken);
+            tbpScaffold.Location = new Point(4, 24);
+            tbpScaffold.Name = "tbpScaffold";
+            tbpScaffold.Padding = new Padding(3);
+            tbpScaffold.Size = new Size(1371, 435);
+            tbpScaffold.TabIndex = 0;
+            tbpScaffold.Text = "Scaffold";
+            tbpScaffold.UseVisualStyleBackColor = true;
+            // 
+            // tbpAccounts
+            // 
+            tbpAccounts.Location = new Point(4, 24);
+            tbpAccounts.Name = "tbpAccounts";
+            tbpAccounts.Padding = new Padding(3);
+            tbpAccounts.Size = new Size(1371, 435);
+            tbpAccounts.TabIndex = 1;
+            tbpAccounts.Text = "Accounts";
+            tbpAccounts.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1379, 485);
-            Controls.Add(btnCallSoap);
+            Controls.Add(tabControl1);
             Controls.Add(statusStrip1);
-            Controls.Add(btnGetAccessToken);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             statusStrip1.ResumeLayout(false);
             statusStrip1.PerformLayout();
+            tabControl1.ResumeLayout(false);
+            tbpScaffold.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -93,5 +132,8 @@
         private StatusStrip statusStrip1;
         private ToolStripStatusLabel toolStripStatusLabel1;
         private Button btnCallSoap;
+        private TabControl tabControl1;
+        private TabPage tbpScaffold;
+        private TabPage tbpAccounts;
     }
 }
